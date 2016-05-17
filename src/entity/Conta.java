9bd626f3e4f108f077b3dfc77bdda44eb5f3f358
Willian Lauber <lcwillian_hd@hotmail.com>
2016-05-16 21:30:00 -0300

    Add files via upload

---
+++ src/entity/Conta.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package entity;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author aluno
+ */
+public class Conta {
+     char data;
+     float total;
+     Paciente_ou_expaciente paciente_ou_expaciente;
+     boolean paga;
+     String descricao;
+     ArrayList <listamedicamento> medicamento_paciente = new ArrayList();
+  
+     int qtdfucionarios;
+     float precofuncionario;
+
+    public void setpaga(boolean paga){
+         this.paga = paga;
+}
+     public String get_name(){
+ return Paciente_ou_expaciente.nome;
+ }
+}
---
+++ src/entity/medicamento.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package entity;
+
+/**
+ *
+ * @author aluno
+ */
+public class medicamento {
+
+    String nome;
+    String tarja;
+    
+    public medicamento(String nome, String tarja) {
+        this.nome = nome;
+        this.tarja = tarja;
+    }
+}

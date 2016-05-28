4b3ca8a69d35bd102f6c7e7393be9eaf60a5c6c2
lcwillian_hd@hotmail.com <29301597891345a>
2016-05-28 11:27:29 -0300

    javadoc

--- src/controller/FXMLDocumentController.java
+++ src/controller/FXMLDocumentController.java
@@ -5,13 +5,14 @@
  */
 package controller;
 
-import java.net.URL;
-import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+
+import java.net.URL;
+import java.util.ResourceBundle;
 
 /**
  *

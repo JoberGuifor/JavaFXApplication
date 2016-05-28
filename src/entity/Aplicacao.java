ec7c237b6efedc9b865f2d384da3884fa20c8043
lcwillian_hd@hotmail.com <29301597891345a>
2016-05-28 11:44:01 -0300

    inicio dos testes, tornou os arquivos executaveis

--- src/entity/Aplicacao.java
+++ src/entity/Aplicacao.java
@@ -5,7 +5,7 @@ public class Aplicacao {
     public static void main(String[] args) {
         Admin admin = new Admin("user01", "user01", "João Agosto Setembro");
         admin.
-
+        AuxiliarInfermagem auxilixar = new AuxiliarInfermagem()
 
     }
 }

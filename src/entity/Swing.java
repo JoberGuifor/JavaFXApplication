797ebf9323a77d6602a62bf957ca1d8534d107a2
lcwillian_hd@hotmail.com <29301597891345a>
2016-05-29 23:21:54 -0300

    teste separados, boundary e entity

--- src/entity/Swing.java
+++ src/entity/Swing.java
@@ -5,16 +5,6 @@
  */
 package entity;
 
-import javafx.application.Application;
-import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.TextField;
-import javafx.scene.effect.Reflection;
-import javafx.scene.layout.HBox;
-import javafx.scene.layout.VBox;
-import javafx.stage.Stage;
-
 import javax.swing.*;
 
 //import javafx.fxml.FXMLLoader;
@@ -27,21 +17,6 @@ import javax.swing.*;
 public class Swing{
 
     public static void main(String[] args) {
-        launch();
-    }
-
-    @Override
-    public void start(Stage palco) throws Exception {
-        final Reflection r = new Reflection();
-        final VBox raiz = new VBox(30);
-        final HBox hbTopo = new HBox(5);
-        final TextField txtNome = new TextField();
-        final Button btnAcao = new Button("Enviar");
-        final Label lblMensagem = new Label();
-        raiz.setTranslateX(10);raiz.setTranslateY(10);
-        lblMensagem.setText("Digite seu nome e clique no botão");
-        hbTopo.getChildren().addAll(txtNome, btnAcao);
-        /////////////////
 
 
         Admin jorge;
@@ -83,13 +58,5 @@ public class Swing{
         paciente.addHistorico_doença("Diabetes");
 
         JOptionPane.showMessageDialog(null, paciente.getHistorico_doenca());
-        ///////////////
-        raiz.getChildren().addAll(hbTopo, lblMensagem);
-        lblMensagem.setEffect(r);
-        Scene cena = new Scene(raiz, 250, 100);
-        palco.setTitle("Aplicação usando código Java");
-        palco.setScene(cena);
-        palco.show();
-
     }
-}
+    }
